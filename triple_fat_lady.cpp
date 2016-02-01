2198b51e124a1826fe63c13e794d5bf989d6169f
Gaurav-Pande <garvpande11235@gmail.com>
2016-02-01 22:08:23 +0530

    Create triple_fat_lady.cpp
    This problem is based on arthmetic. However I am not able to deduce how we came to find the expression as 192 + (k-1)*250. How do we know that it is a geometric progression.

---
+++ triple_fat_lady.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+unsigned long long k,n;
+while(t--){
+cin>>k;
+n = 192 + (k-1)*250;
+cout<<n<<endl;
+}
+return 0;
+ 
+} 
